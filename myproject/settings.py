b18624c011cf2d6e9c495a196d6b2f6a140739b3
Hasib Ahmed <101237375+Hasib105@users.noreply.github.com>
2023-11-01 00:00:32 +0600

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,6 +119,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CART_SESSION_ID = 'cart'
 
 
-STRIPE_PUBLISHABLE_KEY = 'pk_test_51NXypmI4aysZx8DWvx6sWkSYs9CEsMK6wYeFIGowv1SvjUmipKe6Q6ZZsRovaxIFX8uwJJnR2AIZkAUeP7gpiXRv003e9eDeXn'
-STRIPE_SECRET_KEY = 'sk_test_51NXypmI4aysZx8DWSuzDMMB0TdzcML5sN5PYswhYUYNQdnllC5q7B0y3ngL61Wnqefb0K0xOVnHwNFz6QeFZ6oGD008PqvmdX0'
+STRIPE_PUBLISHABLE_KEY = '-----------------------------------------'
+STRIPE_SECRET_KEY = '---------------------------------------------------------------'
 STRIPE_API_VERSION = '2023-10-16' #'2022-08-01'

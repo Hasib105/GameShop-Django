dd2c83966f2fbb08bb501f83fe058609031e8f4a
Hasib Ahmed <hasibahmed1054@gmail.com>
2023-11-14 22:52:16 +0600

    add media file

--- shop/models.py
+++ shop/models.py
@@ -12,6 +12,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    image = models.ImageField(blank=True , null = True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
